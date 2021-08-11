f43a3a6458adb97b61229ab6b348ea3d4aef58d0
Pranav Dave <pranavdave18@gmail.com>
2021-08-11 17:49:46 +0530

    Design Fixes

--- lib/screens/websection/mainview.dart
+++ lib/screens/websection/mainview.dart
@@ -12,6 +12,8 @@ class MainScreenView extends StatefulWidget {
 }
 
 class _MainScreenViewState extends State<MainScreenView> {
+  final String imgUrl =
+      'https://images.unsplash.com/photo-1513628253939-010e64ac66cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3RhcnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60';
 //Hover Bool
   bool isLessonHovered = false;
   bool isPlatformHovered = false;
@@ -40,104 +42,121 @@ class _MainScreenViewState extends State<MainScreenView> {
                         ? 180
                         : MediaQuery.of(context).size.width >= 1201 &&
                                 MediaQuery.of(context).size.width <= 1450
-                            ? 220
-                            : 250,
+                            ? 210
+                            : 230,
                     decoration: BoxDecoration(
-                        gradient: LinearGradient(colors: [
-                      Colors.purple,
-                      Colors.deepPurple,
-                      Colors.blue,
-                      Colors.blueAccent
-                    ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                        image: DecorationImage(
+                            image: NetworkImage(imgUrl), fit: BoxFit.cover)),
                     child: Container(
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          // SizedBox(height: 50.h),
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 30.h),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.book,
-                                    color: Colors.white, size: 20.sp),
-                                SizedBox(width: 5.w),
-                                Text('Learniverse',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 22.sp)),
-                              ],
-                            ),
-                          ),
-                          InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isLesson = true;
-                                  isNav = false;
-                                  isTutoring = false;
-                                  isPlatform = false;
-                                });
+                          Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 30.h),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.book,
+                                        color: Colors.white, size: 20.sp),
+                                    SizedBox(width: 5.w),
+                                    Text('Learniverse',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22.sp)),
+                                  ],
+                                ),
+                              ),
+                              InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isLesson = true;
+                                      isNav = false;
+                                      isTutoring = false;
+                                      isPlatform = false;
+                                    });
 
-                                print('Lesson Clicked ' + isLesson.toString());
-                              },
-                              onHover: (val) {
-                                setState(() {
-                                  isLessonHovered = val;
-                                });
-                              },
-                              child: demoButton(isLessonHovered, 'Lesson',
-                                  context, mainSize)),
-                          InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isPlatform = true;
-                                  isNav = false;
-                                  isLesson = false;
-                                  isTutoring = false;
-                                });
-                                print('Platform Clicked ' +
-                                    isPlatform.toString());
-                              },
-                              onHover: (val) {
-                                setState(() {
-                                  isPlatformHovered = val;
-                                });
-                              },
-                              child: demoButton(isPlatformHovered, 'Platform',
-                                  context, mainSize)),
-                          InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isNav = true;
-                                  isPlatform = false;
-                                  isTutoring = false;
-                                  isLesson = false;
-                                });
-                                print('Nav Clicked ' + isNav.toString());
-                              },
-                              onHover: (val) {
-                                setState(() {
-                                  isNavHovered = val;
-                                });
-                              },
-                              child: demoButton(
-                                  isNavHovered, 'Nav', context, mainSize)),
-                          InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isTutoring = true;
-                                  isNav = false;
-                                  isPlatform = false;
-                                  isLesson = false;
-                                });
-                                print('Tutoring Clicked ' +
-                                    isTutoring.toString());
-                              },
-                              onHover: (val) {
-                                setState(() {
-                                  isTutoringHovered = val;
-                                });
-                              },
-                              child: demoButton(isTutoringHovered, 'Tutoring',
-                                  context, mainSize)),
+                                    print('Lesson Clicked ' +
+                                        isLesson.toString());
+                                  },
+                                  onHover: (val) {
+                                    setState(() {
+                                      isLessonHovered = val;
+                                    });
+                                  },
+                                  child: demoButton(isLessonHovered, 'Lesson',
+                                      context, mainSize)),
+                              InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isPlatform = true;
+                                      isNav = false;
+                                      isLesson = false;
+                                      isTutoring = false;
+                                    });
+                                    print('Platform Clicked ' +
+                                        isPlatform.toString());
+                                  },
+                                  onHover: (val) {
+                                    setState(() {
+                                      isPlatformHovered = val;
+                                    });
+                                  },
+                                  child: demoButton(isPlatformHovered,
+                                      'Platform', context, mainSize)),
+                              InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isNav = true;
+                                      isPlatform = false;
+                                      isTutoring = false;
+                                      isLesson = false;
+                                    });
+                                    print('Nav Clicked ' + isNav.toString());
+                                  },
+                                  onHover: (val) {
+                                    setState(() {
+                                      isNavHovered = val;
+                                    });
+                                  },
+                                  child: demoButton(
+                                      isNavHovered, 'Nav', context, mainSize)),
+                              InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isTutoring = true;
+                                      isNav = false;
+                                      isPlatform = false;
+                                      isLesson = false;
+                                    });
+                                    print('Tutoring Clicked ' +
+                                        isTutoring.toString());
+                                  },
+                                  onHover: (val) {
+                                    setState(() {
+                                      isTutoringHovered = val;
+                                    });
+                                  },
+                                  child: demoButton(isTutoringHovered,
+                                      'Tutoring', context, mainSize)),
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                                margin: EdgeInsets.only(bottom: 25.h),
+                                height: 45.h,
+                                width: 45.w,
+                                child: Center(
+                                  child: Icon(Icons.question_answer_outlined,
+                                      color: Colors.white, size: 18.sp),
+                                ),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(color: Colors.grey),
+                                )),
+                          )
                         ],
                       ),
                     ),
@@ -181,7 +200,7 @@ Widget demoButton(bool val, String text, BuildContext context, double size) {
             : 110,
     decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(28),
-        color: val ? Colors.blue : Colors.transparent),
+        color: val ? Colors.indigoAccent : Colors.transparent),
     child: Container(
       width: MediaQuery.of(context).size.width >= size &&
               MediaQuery.of(context).size.width <= 1200
